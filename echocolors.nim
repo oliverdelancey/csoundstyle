60e3d884fa9446205db5115d9a57daaaf478be5e
Oliver Sandli <56598098+oliversandli@users.noreply.github.com>
2020-11-06 15:13:16 -0600

    Fix indentation

--- echocolors.nim
+++ echocolors.nim
@@ -1,16 +1,16 @@
 import colorize
 
 proc colorEcho(msg: string, darkK, lightK: proc, light = false) =
-    if light:
-        echo msg.lightK
-    else:
-        echo msg.darkK
+  if light:
+    echo msg.lightK
+  else:
+    echo msg.darkK
 
 proc redEcho*(msg: string, light = false) =
-    colorEcho(msg, fgRed, fgLightRed, light)
+  colorEcho(msg, fgRed, fgLightRed, light)
 
 proc yelEcho*(msg: string, light = false) =
-    colorEcho(msg, fgYellow, fgLightYellow, light)
+  colorEcho(msg, fgYellow, fgLightYellow, light)
 
 proc grnEcho*(msg: string, light = false) =
-    colorEcho(msg, fgGreen, fgLightGreen, light)
+  colorEcho(msg, fgGreen, fgLightGreen, light)
